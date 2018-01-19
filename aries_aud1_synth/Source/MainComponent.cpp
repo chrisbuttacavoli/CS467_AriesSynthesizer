9edbb350e7007aa3567146bd1f3454bd5a6b8f27
vdorn <dornv@oregonstate.edu>
2018-01-19 15:39:47 -0800

    Removing all inputs to the main component as we only need outputs currently

--- aries_aud1_synth/Source/MainComponent.cpp
+++ aries_aud1_synth/Source/MainComponent.cpp
@@ -62,7 +62,7 @@ public:
 			setMidiInput(0);
 
         // specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
+        setAudioChannels (0, 2);
 
 
 		//making keyboard visible
