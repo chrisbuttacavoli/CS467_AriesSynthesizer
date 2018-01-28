b4ac7e27c794c689e57a02c8b6fba105b6f07a0e
vdorn <dornv@oregonstate.edu>
2018-01-27 16:21:50 -0800

    Cleaning up main component

--- aries_aud1_synth/Source/MainComponent.cpp
+++ aries_aud1_synth/Source/MainComponent.cpp
@@ -32,7 +32,6 @@ public:
 		isAddingFromMidiInput(false),
 		keyboardComponent(keyboardState, MidiKeyboardComponent::horizontalKeyboard),
 		theSynthProcessor(keyboardState),
-		//synthAudioSource (keyboardState),
 		startTime(Time::getMillisecondCounterHiRes() * 0.001)
     {
 		setOpaque(true);
@@ -40,7 +39,7 @@ public:
 		mySynth.clearVoices();
 
 		theEditor = static_cast <GenericEditor*>(theSynthProcessor.createEditor());
-		//addChildComponent(theEditor);
+
 		addAndMakeVisible(theEditor);
 		theEditor->setSize(800, 400);
 
@@ -75,22 +74,9 @@ public:
         // specify the number of input and output channels that we want to open
         setAudioChannels (0, 2);
 
-
 		//making keyboard visible
 		addAndMakeVisible(keyboardComponent);
 		keyboardState.addListener(this);
-
-		//actually displaying the midi messages
-		/*addAndMakeVisible(midiMessagesBox);
-		midiMessagesBox.setMultiLine(true);
-		midiMessagesBox.setReturnKeyStartsNewLine(true);
-		midiMessagesBox.setReadOnly(true);
-		midiMessagesBox.setScrollbarsShown(true);
-		midiMessagesBox.setCaretVisible(false);
-		midiMessagesBox.setPopupMenuEnabled(true);
-		midiMessagesBox.setColour(TextEditor::backgroundColourId, Colour(0x32ffffff));
-		midiMessagesBox.setColour(TextEditor::outlineColourId, Colour(0x1c000000));
-		midiMessagesBox.setColour(TextEditor::shadowColourId, Colour(0x16000000));*/
 
 		//setting up an audio player to actually output audio
 		audioSourcePlayer.setProcessor(&theSynthProcessor); //for synthProcessor class
@@ -111,20 +97,11 @@ public:
 		keyboardState.removeListener(this);
 		deviceManager.removeMidiInputCallback(MidiInput::getDevices()[midiInputList.getSelectedItemIndex()], this);
 		midiInputList.removeListener(this);
-		//cbOsc1.removeListener(this);
     }
 
     //==============================================================================
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override
     {
-        // This function will be called when the audio device is started, or when
-        // its settings (i.e. sample rate, block size, etc) are changed.
-
-        // You can use this function to initialise any resources you might need,
-        // but be careful - it will be called on the audio thread, not the GUI thread.
-
-        // For more details, see the help for AudioProcessor::prepareToPlay()
-
 		//ignore unused samples to remove garbage from last key press
 		ignoreUnused(samplesPerBlockExpected);
 		lastSampleRate = sampleRate;
@@ -146,14 +123,14 @@ public:
     {
         // (Our component is opaque, so we must completely fill the background with a solid colour)
         g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-		//theEditor->paint(g);
+
         // You can add your drawing code here!
-			Font theFont("Helvetica", "Bold", 50.0f);
-			g.setFont(theFont);
-			g.drawText("Aries Synthesizer", 25, 25, 400, 60, Justification::topRight);
-			Font theFont2("Helvetica", "Bold", 20.0f);
-			g.setFont(theFont2);
-			g.drawText("Chris B, Victoria D, Alex C", 50, 30, 400, 60, Justification::bottomLeft);
+		Font theFont("Helvetica", "Bold", 50.0f);
+		g.setFont(theFont);
+		g.drawText("Aries Synthesizer", 25, 25, 400, 60, Justification::topRight);
+		Font theFont2("Helvetica", "Bold", 20.0f);
+		g.setFont(theFont2);
+		g.drawText("Chris B, Victoria D, Alex C", 50, 30, 400, 60, Justification::bottomLeft);
     }
 
 	//placing objects inside the main window
@@ -163,47 +140,20 @@ public:
         // If you add any child components, this is where you should
         // update their positions.
 		juce::Rectangle<int> area(getLocalBounds());
-		/*midiInputList.setBounds(area.removeFromTop(36).removeFromRight(getWidth() - 150).reduced(8));
-		keyboardComponent.setBounds(area.removeFromBottom(80).reduced(8));
-		midiMessagesBox.setBounds(area.reduced(8));*/
+
 		osc1.setBounds(50, 120, (area.getWidth()/4.8), 125);
 		osc2.setBounds(250, 120, (area.getWidth() / 4.8), 125);
 		osc3.setBounds(450, 120, (area.getWidth() / 4.8), 125);
 		osc4.setBounds(650, 120, (area.getWidth() / 4.8), 125);
-		//theEditor->setBounds(0, 40, (area.getWidth() / 4), 100);
+
 		midiInputList.setBounds(area.removeFromTop(36).removeFromRight(getWidth() - 100).reduced(8));
-		//theEditor->setBounds(area.removeFromTop(50).removeFromRight(getWidth() - 100).reduced(8)); // This didn't give enough height to the component
+		
 		theEditor->setBounds(0, 250, getWidth(), 250); // hardcoded positions
 		keyboardComponent.setBounds(0, 450, getWidth(), 150);
 	}
 
 private:
 	//==============================================================================
-	//MIDI debug prints from: https://juce.com/doc/tutorial_handling_midi_events
-	static String getMidiMessageDescription(const MidiMessage& m)
-	{
-		if (m.isNoteOn())           return "Note on " + MidiMessage::getMidiNoteName(m.getNoteNumber(), true, true, 3);
-		if (m.isNoteOff())          return "Note off " + MidiMessage::getMidiNoteName(m.getNoteNumber(), true, true, 3);
-
-		if (m.isController())
-		{
-			String name(MidiMessage::getControllerName(m.getControllerNumber()));
-
-			if (name.isEmpty())
-				name = "[" + String(m.getControllerNumber()) + "]";
-
-			return "Controller " + name + ": " + String(m.getControllerValue());
-		}
-
-		return String::toHexString(m.getRawData(), m.getRawDataSize());
-	}
-
-	void logMessage(const String& m)
-	{
-		midiMessagesBox.moveCaretToEnd();
-		midiMessagesBox.insertTextAtCaret(m + newLine);
-	}
-
 	/** Starts listening to a MIDI input device, enabling it if necessary. */
 	void setMidiInput(int index)
 	{
@@ -236,7 +186,6 @@ private:
 	{
 		const ScopedValueSetter<bool> scopedInputFlag(isAddingFromMidiInput, true);
 		keyboardState.processNextMidiEvent(message);
-		postMessageToList(message, source->getName());
 	}
 
 	void handleNoteOn(MidiKeyboardState*, int midiChannel, int midiNoteNumber, float velocity) override
@@ -245,7 +194,6 @@ private:
 		{
 			MidiMessage m(MidiMessage::noteOn(midiChannel, midiNoteNumber, velocity));
 			m.setTimeStamp(Time::getMillisecondCounterHiRes() * 0.001);
-			postMessageToList(m, "On-Screen Keyboard");
 		}
 
 		mySynth.noteOn(midiChannel, midiNoteNumber, velocity);
@@ -257,65 +205,11 @@ private:
 		{
 			MidiMessage m(MidiMessage::noteOff(midiChannel, midiNoteNumber));
 			m.setTimeStamp(Time::getMillisecondCounterHiRes() * 0.001);
-			postMessageToList(m, "On-Screen Keyboard");
 		}
-	}
-
-	// This is used to dispach an incoming message to the message thread
-	class IncomingMessageCallback : public CallbackMessage
-	{
-	public:
-		IncomingMessageCallback(MainContentComponent* o, const MidiMessage& m, const String& s)
-			: owner(o), message(m), source(s)
-		{}
-
-		void messageCallback() override
-		{
-			if (owner != nullptr)
-				owner->addMessageToList(message, source);
-		}
-
-		Component::SafePointer<MainContentComponent> owner;
-		MidiMessage message;
-		String source;
-	};
-
-	void postMessageToList(const MidiMessage& message, const String& source)
-	{
-		(new IncomingMessageCallback(this, message, source))->post();
-	}
-
-	void addMessageToList(const MidiMessage& message, const String& source)
-	{
-		const double time = message.getTimeStamp() - startTime;
-
-		const int hours = ((int)(time / 3600.0)) % 24;
-		const int minutes = ((int)(time / 60.0)) % 60;
-		const int seconds = ((int)time) % 60;
-		const int millis = ((int)(time * 1000.0)) % 1000;
-
-		const String timecode(String::formatted("%02d:%02d:%02d.%03d",
-			hours,
-			minutes,
-			seconds,
-			millis));
-
-		const String description(getMidiMessageDescription(message));
-
-		const String midiMessageString(timecode + "  -  " + description + " (" + source + ")"); // [7]
-		logMessage(midiMessageString);
 	}
 
 
 	void addOscillatorControls() {
-		/*addAndMakeVisible(lblOsc1);
-		lblOsc1.setText("Oscillator 1:", dontSendNotification);
-		lblOsc1.attachToComponent(&cbOsc1, true);
-		addAndMakeVisible(cbOsc1);
-		cbOsc1.addItem("Sine", sine);
-		cbOsc1.addItem("Saw", saw);
-		cbOsc1.addListener(this);
-		cbOsc1.setSelectedId(sine);*/
 		addAndMakeVisible(osc1);
 		addAndMakeVisible(osc2);
 		addAndMakeVisible(osc3);
@@ -332,11 +226,8 @@ private:
 	bool isAddingFromMidiInput;                 // [4]
 	MidiKeyboardState keyboardState;            // [5]
 	MidiKeyboardComponent keyboardComponent;    // [6]
-	TextEditor midiMessagesBox;
 	double startTime;
 
-	//AudioSourcePlayer audioSourcePlayer;
-	//SynthAudioSource synthAudioSource;
 	AudioProcessorPlayer audioSourcePlayer;	//need to play audio through a processor player now
 	SynthProcessor theSynthProcessor;		//new synth processor to replay synthAudioSource
 	GenericEditor *theEditor;
@@ -345,8 +236,6 @@ private:
 	double lastSampleRate;
 
 	// Controls
-	/*ComboBox cbOsc1;
-	Label lblOsc1;*/
 	OscillatorCombo osc1;
 	OscillatorCombo osc2;
 	OscillatorCombo osc3;
